9b17120064a7b9814df5b2a617295fd966f0cf9f
Aaron Powell <me@aaron-powell.com>
2019-10-14 21:05:28 +1100

    Adding some styles

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { IAgendaItem, groupAgendaTimeslots } from "../agendaUtils";
 import TimeSlot from "./Timeslot";
+import styles from "styled-components";
+
+const DayContainer = styles.div`
+  border: 1px solid #cdcdcd;
+  margin: 40px;
+  border-radius: 10px 10px 0px 0px;
+`;
+
+const Title = styles.h2`
+  text-transform: capitalize;
+`;
 
 interface IDayProps {
   day: string;
@@ -11,12 +22,12 @@ const Day: React.FC<IDayProps> = ({ day, agenda }) => {
   const timeslots = groupAgendaTimeslots(agenda);
 
   return (
-    <div className={`Day ${day}`}>
-      <h2>{day}</h2>
+    <DayContainer>
+      <Title>{day}</Title>
       {Object.keys(timeslots).map(key => (
         <TimeSlot key={key} timeslot={key} talks={timeslots[key]} />
       ))}
-    </div>
+    </DayContainer>
   );
 };
 
