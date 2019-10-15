a4d574c2353d56660e8728775ab86d1dd61a3c35
Aaron Powell <me@aaron-powell.com>
2019-10-15 11:02:00 +1100

    Styling the talk card

--- src/components/Talk.tsx
+++ src/components/Talk.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import styles from "styled-components";
 
 interface ITalkProps {
   title: string;
   speaker: string;
 }
 
+const Container = styles.div`
+  width: 200px;
+  display: inline-block;
+  vertical-align: top;
+  padding: 5px;
+  margin: 5px;
+  border: 1px solid #BADA55;
+  border-radius: 5px;
+  min-height: 100px;
+  box-shadow: 5px 10px 8px #888888;
+  cursor: pointer;
+`;
+
 const Talk: React.FC<ITalkProps> = ({ title, speaker }) => (
-  <p>
-    {title} - {speaker}
-  </p>
+  <Container>
+    <em>{title}</em>
+    <br />
+    <strong>{speaker}</strong>
+  </Container>
 );
 
 export default Talk;
